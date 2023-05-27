2d0e2bc50f9960e10fb568b3d0c80c1a690b453d
Xiang Que <65532999@qq.com>
2023-05-27 10:33:27 -0700

    Initial commit

---
+++ R/OpenMindat.R
@@ -0,0 +1,522 @@
+############ Initializing API Call ############
+mindat_connection <- function(token, base_url = "https://api.mindat.org",fmt ="json"){
+  set_api_token(token)
+  mindat_setup(base_url)
+}
+############ Initializing API Call ############
+
+############  mindat_cache.R  #################
+mindat_cache <- new.env()
+
+mindat_cache_set<-function(varname, value)
+{
+  assign(varname, value, envir = mindat_cache)
+}
+
+mindat_cache_get<-function(varname)
+{
+  return (get(varname, envir=mindat_cache))
+}
+
+mindat_cache_delete<-function(varname)
+{
+  assign(varname, NULL, envir = mindat_cache)
+}
+
+mindt_cache_empty<-function(){
+  rm(list = ls(envir = mindat_cache))
+}
+
+mindat_cache_has <-function(varname)
+{
+  if(exists(varname, envir= mindat_cache)){
+
+    return (!is.null(mindat_cache_get(varname)))
+  }
+  return (FALSE)
+}
+
+mindat_cache_return_or_setup<-function(varname, setupfun)
+{
+  if(!mindat_cache_has(varname))
+  {
+    if(is.function(setupfun)){
+      setupcall <- as.call(list(setupfun))
+    } else {
+      setupcall <- call(setupfun)
+    }
+    setupvalue <- eval(setupcall)
+    mindat_cache_set(varname, setupvalue)
+    mindat_cache_get(varname)
+  }
+  return (mindat_cache_get(varname))
+}
+########### mindat_cache.R ###########
+
+########### mindat_api_tools.R #############
+default_uri_builder<-function(api_base_uri,config, ..., querystring = ''){
+  endpoint_base <- config[['endpoint_base']]
+  uri <- paste(api_base_uri, '/', endpoint_base, sep = "")
+  if(querystring != ''){
+    uri <- paste(uri, querystring, sep="?")
+  }
+  uri
+}
+
+mindat_api_endpoint<-function(name, endpoint_base, ..., uri_builder = default_uri_builder,query_params = list()){
+  if(!is.function(uri_builder)){
+    stop("uri_builder must be a function")
+  }
+
+  config <- c(list(...), endpoint_base = endpoint_base, uri_builder = uri_builder,
+              query_params = query_params)
+
+  api_end_points<-mindat_cache_return_or_setup('api_end_points', function(){
+    return (list())
+  })
+
+  api_end_points[[name]] <- config
+
+  mindat_cache_set('api_end_points', api_end_points)
+}
+
+build_uri<-function(endpoint, ..., query = list(), api_base = NULL){
+  # passed or global
+  if(is.null(api_base)){
+    if(mindat_cache_has('api_base')){
+      api_base <- mindat_cache_get('api_base')
+    } else {
+      stop("No API base path was configured")
+    }
+  }
+
+  api_end_points<-mindat_cache_get('api_end_points')
+  config <- api_end_points[[endpoint]]
+  if(is.null(config)){
+    stop(sprintf("No config for endpoint '%s' is registered", endpoint))
+  }
+
+  query <- c(query, list(...))
+
+  # check compulsory params for end point
+  if(!is.null(config['query_params']) && length(config['query_params'][[1]] > 0)){
+    .stop_on_missing_param(config['query_params'][[1]], query)
+  }
+  qs<-mindat_build_querystring(query)
+  builder <- config[['uri_builder']]
+  uri <- builder(api_base, config, querystring = qs)
+  uri
+}
+set_api_base<-function(api_base){
+  mindat_cache_set('api_base', api_base)
+}
+
+set_api_token<-function(api_token){
+  mindat_cache_set('api_token', api_token)
+}
+
+stop_missing_param<-function(compulsory_params, query){
+  q_params <- names(query)
+  for(c_param in compulsory_params) {
+    if(!is.element(c_param, q_params)){
+      stop(sprintf("Query string param '%s' is missing", c_param))
+    }
+  }
+}
+########### mindat_api_tools.R #############
+
+
+########### mindat_rest_api.R ###########
+mindat_uri_builder<- function(api_base_url, config, querystring = ''){
+  if(querystring != ''){
+    uri <- paste(api_base_url, '/', sprintf(config[['endpoint_base']], querystring) ,sep = "")
+  }
+  else{
+    config_str <- sub('%s','',config[['endpoint_base']])
+    uri <- paste(api_base_url,config_str,sep = "/")
+  }
+  uri
+}
+
+mindat_set_up_endpoints<-function(){
+  # single ima mineral
+  mindat_api_endpoint('minerals_ima', 'minerals_ima/%s', uri_builder = mindat_uri_builder,
+                     compulsory_params = list('id'))
+
+  # ima minerals list
+  mindat_api_endpoint('minerals_ima_list', 'minerals_ima/%s', uri_builder = mindat_uri_builder)
+
+  # single locality
+  mindat_api_endpoint('localities', 'localities/%s',uri_builder = mindat_uri_builder,
+                     compulsory_params = list('id'))
+  # localities
+  mindat_api_endpoint('localities_list', 'localities/%s',uri_builder = mindat_uri_builder)
+
+  # locality_age
+  mindat_api_endpoint('locality_age_list', 'locality_age/%s',uri_builder = mindat_uri_builder)
+  mindat_api_endpoint('locality_age', 'locality_age/%s',uri_builder = mindat_uri_builder,
+                      compulsory_params = list('id'))
+
+  # locality_status
+  mindat_api_endpoint('locality_status_list', 'locality_status/%s',uri_builder = mindat_uri_builder)
+  mindat_api_endpoint('locality_status', 'locality_status/%s',uri_builder = mindat_uri_builder,
+                      compulsory_params = list('id'))
+
+  mindat_api_endpoint('locality_type_list', 'locality_type/%s',uri_builder = mindat_uri_builder)
+  mindat_api_endpoint('locality_type', 'locality_type/%s',uri_builder = mindat_uri_builder,
+                      compulsory_params = list('id'))
+
+  #geomaterials
+  mindat_api_endpoint('geomaterials', 'geomaterials/%s',uri_builder = mindat_uri_builder,
+                      compulsory_params = list('id'))
+  #geomaterials_list
+  mindat_api_endpoint('geomaterials_list', 'geomaterials/%s',uri_builder = mindat_uri_builder)
+
+  #geomaterials_varieties/{id}/varieties/
+  mindat_api_endpoint('geomaterials_varieties', 'geomaterials/%s/varieties',uri_builder = mindat_uri_builder)
+
+  #geomaterials/dict/
+  mindat_api_endpoint('geomaterials/dict', 'geomaterials/dict/%s',uri_builder = mindat_uri_builder)
+
+  #geomaterials_search/
+  mindat_api_endpoint('geomaterials_search', 'geomaterials_search/%s',uri_builder = mindat_uri_builder)
+
+  #dana-8
+  mindat_api_endpoint('dana-8', 'dana-8/%s',uri_builder = mindat_uri_builder,
+                      compulsory_params = list('id'))
+
+  mindat_api_endpoint('dana-8', 'dana-8/%s',uri_builder = mindat_uri_builder)
+
+  #dana-8/groups
+  mindat_api_endpoint('dana-8/groups', 'dana-8/groups/%s',uri_builder = mindat_uri_builder,
+                      compulsory_params = list('id'))
+  #dana-8/subgroups
+  mindat_api_endpoint('dana-8/subgroups', 'dana-8/subgroup/%s',uri_builder = mindat_uri_builder)
+
+  #countries
+  mindat_api_endpoint('countries', 'countries/%s',uri_builder = mindat_uri_builder)
+
+  #countries
+  mindat_api_endpoint('countries', 'countries/%s',uri_builder = mindat_uri_builder,
+                      compulsory_params = list('id'))
+}
+mindat_setup<-function(bas_uri = 'https://api.mindat.org'){
+  set_api_base(bas_uri)
+  mindat_set_up_endpoints()
+  mindat_cache_set('api_format', 'json')
+}
+
+
+########### mindat_rest_api.R ###########
+
+########## mindat_network.R ###########
+collapse_array_columns_map<- function (element){
+  if (length (element) > 1){
+    mapped<- paste (element, collapse=";")
+  }else {
+    mapped<- element
+  }
+  mapped
+}
+
+mindat_make_data_frame<-function(reg_list){
+  if (is.list(reg_list)){
+    #df_out <- data.frame(reg_list)
+    df_out <-as.data.frame(do.call(cbind,reg_list))
+  }
+  else{
+     mineralcnt <-reg_list$count
+     data_results<-reg_list$results
+     df_out <- data.frame(data_results)
+     next_url<- reg_list$`next`
+     while(!is.null(next_url)){
+       query<-list(format= "json")
+       all_data2<-GET(next_url,add_headers('Authorization'= paste('Token ',YOUR_API_KEY,sep = "")),query = query)
+       str(content(all_data2))
+       all_data2_text <- content(all_data2,"text", encoding = "UTF-8")
+       all_data2_json <- fromJSON(all_data2_text,flatten = TRUE)
+       data_results2<-all_data2_json$results
+       df_tmp = data.frame(data_results2)
+       df_out <- rbind(df_out,df_tmp)
+       next_url<- all_data2_json$`next`
+     }
+  }
+  df_out
+}
+mindat_parse_raw_data<-function(raw_data){
+  data_list <- fromJSON(raw_data)
+  if ("results" %in% names(data_list) ){
+        if(mindat_cache_has('api_token')){
+          api_token <- mindat_cache_get('api_token')
+        } else {
+          stop("No API token configured")
+        }
+        data_results<-data_list$results
+        df_out <- data.frame(data_results)
+        next_url<- data_list$`next`
+        while(!is.null(next_url)){
+          query<-list(format= "json")
+          all_data2<-GET(next_url,add_headers('Authorization'= paste('Token ',api_token,sep = "")),query = query)
+          str(content(all_data2))
+          all_data2_text <- content(all_data2,"text", encoding = "UTF-8")
+          all_data2_json <- fromJSON(all_data2_text,flatten = TRUE)
+          data_results2<-all_data2_json$results
+          df_tmp = data.frame(data_results2)
+          df_out <- rbind(df_out,df_tmp)
+          next_url<- all_data2_json$`next`
+        }
+        df_out
+  }
+  else{
+    df_out <- mindat_make_data_frame(data_list)#data_list[["records"]]
+    df_out
+  }
+}
+mindat_extract_response_body<-function(response){
+  if (200 == status_code(response)){
+    # Content in the API
+    str(content(response))
+    # Converting content to text
+    all_data_text <- content(response,"text", encoding = "UTF-8")
+  }
+  else{
+    stop(sprintf('Error in API: The server return a status %s,which indicates that something went wrong with your request.',status_code(response)))
+  }
+}
+
+mindat_get_data_from_uri<-function(uri){
+  if(mindat_cache_has('api_token')){
+    api_token <- mindat_cache_get('api_token')
+  } else {
+    stop("No API token configured")
+  }
+  response <- GET(uri,add_headers('Authorization'= paste('Token ',api_token,sep = "")))
+  raw_data <- mindat_extract_response_body(response)
+  df<-mindat_parse_raw_data(raw_data)
+  df
+}
+
+mindat_build_querystring<-function(args){
+  qs <- ''
+  for (argName in names(args)) {
+    strArgValue <- as.character(args[argName][[1]])
+    encodedArgValue <-  URLencode(strArgValue)
+    if (argName == "id"){
+      qs <-paste(qs, encodedArgValue, '&', sep = "")
+      #break
+    }
+    else if(argName == "ids"){
+      qs <-paste('?id__in=',qs, encodedArgValue, '&', sep = "")
+    }
+    else{
+      qs <- paste(qs, argName, "=", encodedArgValue, '&', sep = "")
+    }
+  }
+  qs <- substr(qs,0,nchar(qs)-1)
+  qs
+}
+
+########## mindat_network.R ###########
+
+########### mindat_querys.R #############
+mindat_query<-function(endpoint, query = list()){
+  query <- lapply(query, params_to_string)
+  uri <- build_uri(endpoint, query = query)
+  df <- mindat_get_data_from_uri(uri)
+  df
+}
+
+params_to_string<-function(params){
+  if(!(is.vector(params))){
+    stop("Vector expected")
+  }
+  if(length(params) > 1){
+    str <- params[[1]]
+    for (p in params[2:length(params)]) {
+      str <- paste(str, ",", p, sep = "")
+    }
+  } else {
+    str <- params
+  }
+  return (str)
+}
+
+mindat_mineral_ima<-function(id, ...){
+  l<-list(...)
+  mindat_query('minerals_ima', query = c(list(id = id), l))
+}
+
+mindat_mineral_ima_list<-function(...){
+  l<-list(...)
+  mindat_query('minerals_ima_list', query = l)
+}
+
+mindat_localities<-function(...){
+  l<-list(...)
+  mindat_query('localities', query = l)
+}
+
+mindat_localitiy<-function(id,...){
+  l<-list(...)
+  mindat_query('localities', query = c(list(id = id), l))
+  #mindat_query('localities', query = c(list(id = id), l),compulsory_params = list('id'))
+}
+
+mindat_locality_age<-function(id,...){
+  l<-list(...)
+  #mindat_query('locality_age', query = c(list(id = id), l),compulsory_params = list('id'))
+  mindat_query('locality_age', query = c(list(id = id), l))
+}
+
+mindat_locality_ages<-function(...){
+  l<-list(...)
+  mindat_query('locality_age', query = l)
+}
+
+mindatnat_locality_status<-function(id,...){
+  l<-list(...)
+  mindat_query('locality_status', query = c(list(id = id), l))
+}
+
+mindat_locality_status_list<-function(...){
+  l<-list(...)
+  mindat_query('locality_status_list', query = l)
+}
+
+mindat_locality_type_list<-function(...){
+  l<-list(...)
+  mindat_query('locality_type_list',l)
+}
+
+mindat_locality_type<-function(id,...){
+  l<-list(...)
+  mindat_query('locality_type', query = c(list(id = id), l))
+}
+
+mindat_geomaterial<-function(id,...){
+  l<-list(...)
+  mindat_query('geomaterials', query  = c(list(id = id), l))
+  # mindat_query('geomaterials', query  = c(list(id = id), l),compulsory_params = list('id'))
+}
+
+mindat_geomaterial_varieties<-function(id,...){
+  l<-list(...)
+  mindat_query('geomaterials_varieties', query  = c(list(id = id), l))
+  # mindat_query('geomaterials', query  = c(list(id = id), l),compulsory_params = list('id'))
+}
+
+mindat_geomaterial_list<-function(...){
+  l<-list(...)
+  # if(length(l)){
+  #   print(l)
+  # }
+  mindat_query('geomaterials_list', query = l)
+}
+
+mindat_country<-function(id,...){
+  l<-list(...)
+  mindat_query('countries', query  = c(list(id = id), l))#,compulsory_params = list('id')
+}
+
+mindat_countries<-function(...){
+  l<-list(...)
+  mindat_query('countries', query = l)
+}
+
+# mindat_mineral_geo<-function(){
+#
+# }
+# mindat_geo_minerals<-function(){
+#
+# }
+
+########### mindat_querys.R #############
+
+########### mindat_geomaterials_tools.R #############
+geomaterials_contain_all_elems<- function(icl_elms_vector,...){
+  df <- mindat_geomaterial_list(ids = c(''),elements_inc = icl_elms_vector,...)
+  df
+}
+geomaterials_without_elems <- function(ecl_elms_vector,...){
+  df <- mindat_geomaterial_list(ids = c(''),elements_exc = ecl_elms_vector,...)
+  df
+}
+geomaterials_contain_all_and_without_elems <- function(icl_elm_vector,ecl_elms_vector,...){
+  df <- mindat_geomaterial_list(ids = c(''),elements_inc = icl_elm_vector,elements_exc = ecl_elms_vector,...)
+  df
+}
+
+geomaterials_contain_any_elems <- function(any_elems,...){
+  df_out <- data.frame()
+  for (elem in any_elems){
+    df <- mindat_geomaterial_list(ids = c(''),elements_inc = c(elem),...)
+    df_out <- rbind(df_out,df)
+  }
+  df_out
+}
+
+geomaterials_cleavagetype <- function(types,...){
+  if(length(types)>1){
+    # merge_types<- paste(types,"&",sep = "",collapse ='')
+    # merge_types <- substr(merge_types,0,nchar(merge_types)-1)
+    # df <- mindat_geomaterial_list(ids = c(''),cleavagetype = merge_types,...)
+    df_out <- data.frame()
+    for (type in types){
+      df <- mindat_geomaterial_list(ids = c(''),cleavagetype = c(type),...)
+      df_out <- rbind(df_out,df)
+    }
+    df_out
+  }
+  else{
+    df_out <- mindat_geomaterial_list(ids = c(''),cleavagetype = types,...)
+    df_out
+  }
+}
+
+geomaterials_colour<- function(colors,...){
+  if(length(colors)>1){
+    df_out <- data.frame()
+    for (color in colors){
+      df <- mindat_geomaterial_list(ids = c(''),colour = c(color),...)
+      df_out <- rbind(df_out,df)
+    }
+    df_out
+  }
+  else{
+    df_out <- mindat_geomaterial_list(ids = c(''),colour = colors,...)
+    df_out
+  }
+}
+
+geomaterials_crystal_system <- function(crystals,...){
+  if(length(crystals)>1){
+    df_out <- data.frame()
+    for (crystal in crystals){
+      df <- mindat_geomaterial_list(ids = c(''),crystal_system = c(crystal),...)
+      df_out <- rbind(df_out,df)
+    }
+    df_out
+  }
+  else{
+    df_out <- mindat_geomaterial_list(ids = c(''),crystal_system = crystals,...)
+    df_out
+  }
+}
+
+geomaterials_dens_greater_than<- function(gt,...){
+  df_out <- mindat_geomaterial_list(ids = c(''),density__from = gt,...)
+  df_out
+}
+
+geomaterials_dens_less_than<- function(lt,...){
+  df_out <- mindat_geomaterial_list(ids = c(''),density__to = lt,...)
+  df_out
+}
+
+geomaterials_dens_range <- function(gt,lt,...){
+  df_out <- mindat_geomaterial_list(ids = c(''),density__from = gt,density__to = lt,...)
+  df_out
+}
+
+
+########### mindat_geomaterials_tools.R #############
---
+++ test/test_openmindat.R
@@ -0,0 +1,179 @@
+# install.packages("httr")
+# install.packages("jsonlite")
+library(OpenMindat)
+library(httr)
+library(jsonlite)
+library(testthat)
+
+dc <- c("1","2","3")
+for (i in dc){
+  print(i)
+}
+###test all basic mindat query#####
+# Initializing API Call
+#test_base_uri = "https://api.mindat.org"
+test_base_token = "aa9c25fa95d8063908cb2bf186c9e79f"
+mindat_connection(test_base_token)
+response <- mindat_geomaterial(id=6)
+response_fields<- mindat_geomaterial(id = 6)
+#response_fields_ids<- mindat_geomaterial_list(ids = c('6','7','8'),fields ="id,name,updttime,ima_formula,ima_status,ima_notes,key_elements",elements_inc = "H,O")#ids = c('6','7','8'),
+
+df_contains_all <- geomaterials_contain_all_elems(c('Be','Cr'))
+df_contains_any <- geomaterials_contain_any_elems(c('Be','Cr'))
+df_notcontains <-  geomaterials_notcontain_elements(c('H','O'))
+df_contain_butnot <- geomaterials_contain_elems_butnot_elems(c('Be','O'),c('H'))
+df_cleavagetype <- geomaterials_cleavagetype(c("None Observed"))
+df_cleavagetype2 <- geomaterials_cleavagetype(c("None Observed","Imperfect/Fair"))
+df_color <- geomaterials_colour(c("yellow"))
+df_color2 <- geomaterials_colour(c("yellow","brown"))
+df_crystal <-geomaterials_crystal_system(c("Amorphous"))
+df_crystal2 <-geomaterials_crystal_system(c("Hexagonal"))
+df_crystal3 <-geomaterials_crystal_system(c("Amorphous","Hexagonal"))
+df_density_gt <- geomaterials_dens_greater_than(3)
+df_density_lt <- geomaterials_dens_less_than(0.2)
+df_density_range <- geomaterials_dens_range(3,3.5)
+
+
+
+response <- mindat_geomaterial_list(ids = c('3','5','7','9','222'))
+response <- mindat_geomaterial_list()
+response_varieties <- mindat_geomaterial_varieties(id=8)
+
+
+
+response2 <- mindat_mineral_ima(id=1)
+id_list2<-c('1','2','3','4','6')
+response2_list <- mindat_mineral_ima_list(ids =id_list2)
+response3_list <- mindat_mineral_ima_list(ids =id_list2,fields = "id,name,ima_formula,ima_symbol")
+response3 <- mindat_localitiy(id = 6)
+response4 <- midnat_country(id = 6)
+response5 <- midnat_locality_age(id = 6)
+response6 <- midnat_locality_status(id = 6)
+response7 <- midnat_locality_type(id = 212)
+###test all basic mindat query#####
+
+test_that("Well formed URIs with default builder and no params", {
+  mindat_api_endpoint('geomaterials', 'geomaterials/')
+  expect_equal(build_uri('geomaterials', api_base = test_base_uri), 'https://api.mindat.org/geomaterials/')
+})
+
+test_that("Well formed URIs with default builder with params as list", {
+  q<-list(1)
+  expect_equal(build_uri('geomaterials', api_base = test_base_uri, query = q), 'https://api.mindat.org/geomaterials/')
+})
+
+
+test_that("Well formed URIs with default builder and params as ecliptic parameters", {
+  expect_equal(build_uri('testend1', api_base = test_base_uri, p1 = 'one', p2 = 'two'), 'https://api.mindat.org/controller1?p1=one&p2=two')
+})
+
+test_that("Error if no config for an endpoint", {
+  expect_error(build_uri('testend_nonexistent'))
+})
+
+test_that("Globally setted api_base leads to correct URI", {
+  mindat_api_endpoint('testend2', 'controller2')
+  mindat_cache_delete('api_base')
+  set_api_base(test_base_uri)
+  expect_equal(build_uri('testend2'), 'https://api.mindat.org/controller2')
+})
+
+
+# test on compulsory query string params
+
+test_that("Error if a an expected query string param is missing", {
+  mindat_api_endpoint('testend3', 'controller3', query_params = list('mustbe'))
+
+  expect_error(build_uri('testend3'))
+})
+
+
+test_that("Error if uri_builder not a not a function", {
+  # some cases
+  expect_error(mindat_api_endpoint('testend4', 'controller4', uri_builder = NULL))
+  expect_error(mindat_api_endpoint('testend4', 'controller4', uri_builder = "wrongBuilder"))
+  expect_error(mindat_api_endpoint('testend4', 'controller4', uri_builder = list("a")))
+})
+###test mindat_api ####
+
+
+
+###test parse api ####
+bad_http_response <- 'HTTP/1.1 400 OK\r\nContent-Type: application/json; charset=utf-8\r\nVary: Accept-Encoding,User-Agent\r\n\r\nTHE BODY'
+good_http_response <- 'HTTP/1.1 200 OK\r\nContent-Type: application/json; charset=utf-8\r\nVary: Accept-Encoding,User-Agent\r\n\r\nTHE BODY'
+
+# network tests
+test_that("Error on non 200 status response", {
+  expect_error(mindat_extract_response_body(bad_http_response))
+})
+
+test_that("Body extraction on successful request", {
+  expect_equal(mindat_extract_response_body(good_http_response), 'THE BODY')
+})
+
+# PARSING
+
+# response to
+# "http://paleobiodb.org/data1.1/occs/list.json?base_name=Alopex&interval=Quaternary&show=loc,time"
+# 1/2/2014
+# 7 records
+# record 199941 includes extra column cny
+api_json_response_sample <- "{\"records\": [{\"oid\":198254,\"typ\":\"occ\",\"cid\":20434,\"tna\":\"Alopex lagopus\",\"rnk\":3,\"tid\":43911,\"eag\":1.80000,\"lag\":0.30000,\"rid\":[2048],\"cc2\":\"Canada\",\"sta\":\"Yukon\",\"cxi\":33,\"ein\":740,\"lin\":923},{\"oid\":198265,\"typ\":\"occ\",\"cid\":20436,\"tna\":\"Alopex lagopus\",\"rnk\":3,\"tid\":43911,\"eag\":1.80000,\"lag\":0.30000,\"rid\":[2048],\"cc2\":\"Canada\",\"sta\":\"Yukon\",\"cxi\":33,\"ein\":740,\"lin\":923},{\"oid\":199751,\"typ\":\"occ\",\"cid\":20626,\"tna\":\"Alopex lagopus\",\"rnk\":3,\"tid\":43911,\"eag\":0.12600,\"lag\":0.01170,\"rid\":[1784],\"cc2\":\"Canada\",\"sta\":\"Yukon\",\"cxi\":922,\"ein\":922,\"lin\":922},{\"oid\":199941,\"typ\":\"occ\",\"cid\":20643,\"tna\":\"Alopex lagopus\",\"rnk\":3,\"tid\":43911,\"eag\":0.12600,\"lag\":0.01170,\"rid\":[2872],\"cc2\":\"United States\",\"sta\":\"Alaska\",\"cny\":\"North Slope\",\"cxi\":922,\"ein\":922,\"lin\":922},{\"oid\":373590,\"typ\":\"occ\",\"cid\":35359,\"tna\":\"Alopex\",\"rnk\":5,\"tid\":41193,\"eag\":5.33300,\"lag\":0.01170,\"rid\":[9534],\"cc2\":\"Hungary\",\"sta\":\"Barany\",\"cxi\":1,\"ein\":34,\"lin\":33},{\"oid\":485281,\"typ\":\"occ\",\"cid\":48587,\"tna\":\"Alopex lagopus\",\"rnk\":3,\"tid\":43911,\"eag\":0.12600,\"lag\":0.01170,\"rid\":[12964],\"cc2\":\"Russian Federation\",\"sta\":\"Respublika Saha (Jakutija)\",\"cxi\":922,\"ein\":922,\"lin\":922},{\"oid\":766675,\"typ\":\"occ\",\"cid\":81830,\"tna\":\"Alopex lagopus\",\"rnk\":3,\"tid\":43911,\"eag\":0.12600,\"lag\":0.01170,\"rid\":[27585],\"cc2\":\"United States\",\"sta\":\"Alaska\",\"cxi\":922,\"ein\":922,\"lin\":922}]}"
+
+api_json_response_sample_returns <- '{\n"records": [\n{"oid":1001,"typ":"occ","cid":160,"tna":"Wellerella","rnk":5,"tid":29018,"oei":"Missourian","eag":305.90000,"lag":303.40000,"rid":[12]}\n]\n}\n'
+
+test_that(".parse_raw_data for a JSON string response is a data.frame", {
+
+  parsed <- mindat_parse_raw_data(api_json_response_sample)
+  expect_is(parsed, 'data.frame')
+})
+
+test_that("parse_raw_data for a JSON string that includes \n yields just one row in the dataframe", {
+
+  parsed <- mindat_parse_raw_data(api_json_response_sample_returns)
+  expect_equal(dim(parsed)[1], 10)
+})
+
+# test_that("A JSON string with different columns for some occurency resolves to a data.frame comprising all the columns", {
+#
+#   expected_names <- c("oid", "typ", "cid", "tna", "rnk", "tid", "eag", "lag", "rid", "cc2", "sta", "cxi", "ein", "lin")  #oei, "cny"
+#   resp_df<-mindat_parse_raw_data(api_json_response_sample)
+#   resp_names <- names(resp_df)
+#
+#   expect_identical(resp_names, expected_names)
+# })
+
+
+###test parse api ####
+###test mindat query ####
+
+test_that("Param with several (as list) values joins to csv ok", {
+  expect_equal(params_to_string(list("aa","bb")), 'aa,bb')
+})
+
+test_that("Param with several (as character vector) values joins to csv ok", {
+  expect_equal(params_to_string(c("aa","bb")), 'aa,bb')
+})
+
+test_that("Param with one value remains", {
+  expect_equal(params_to_string("aa"), "aa")
+})
+
+###test mindat query#####
+
+
+###test all basic mindat query#####
+test_that("mindat_mineral output is a dataframe, and the names are characters", {
+  set_api_token(test_base_token)
+  response<- mindat_mineral (id=1)
+  # mindat_cache_has('token')
+  expect_true(is.data.frame (response))
+  expect_is (names (response)[1], "character")
+  expect_true (dim (response)[1]==1)
+})
+
+###test all basic mindat query#####
+
+
+
+
