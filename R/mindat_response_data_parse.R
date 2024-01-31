a5c89f6d79068e620ba4d0b1c608f238f60d83fa
Xiang Que <65532999@qq.com>
2024-01-31 12:23:20 -0800

    update examples in response_parse

--- R/mindat_response_data_parse.R
+++ R/mindat_response_data_parse.R
@@ -36,6 +36,15 @@ usethis::use_import_from("utils", "write.table")
 #' @usage mindat_make_data_frame (reg_list)
 #' @param reg_list response json data to list format obj.
 #' @return df_out, R data frame
+#' @examples
+#' \dontrun{
+#' id<- c('42155','9300','11282','48322')
+#' name<-  c('Cuarzo opalescente', 'Cupromagnesite', 'Cuprozippeite', 'Quartz-anorthosite')
+#' ima_status <- c(0,0,0,0)
+#' synid <- c(42133, 9281, 0, 0)
+#' list_cvt <- list(id=id, name=name, ima_status=ima_status, synid=synid)
+#' df<- mindat_make_data_frame(list_cvt)
+#' }
 #' @export
 mindat_make_data_frame<-function(reg_list){
   if (is.list(reg_list)){
@@ -70,6 +79,13 @@ mindat_make_data_frame<-function(reg_list){
 #' @usage mindat_parse_raw_data (raw_data)
 #' @param raw_data content of the response body
 #' @return df_out, R data frame
+#' @examples
+#' \dontrun{
+#' rd<-"{\"count\":5,\"next\":null,\"previous\":null,
+#' \"results\":[{\"name\":\"Diamond\"},{\"name\":\"Khamrabaevite\"},
+#' {\"name\":\"Moissanite\"},{\"name\":\"Qingsongite\"},{\"name\":\"Uakitite\"}]}"
+#' df<- mindat_parse_raw_data(rd)
+#' }
 #' @export
 mindat_parse_raw_data<-function(raw_data){
   data_list <- fromJSON(raw_data)
@@ -109,6 +125,15 @@ mindat_parse_raw_data<-function(raw_data){
 #' @param response response json
 #' @return if status of the response is sucess (200),return the all_data_text(the content of response).
 #' Otherwise,report the errors.
+#' @examples
+#' \dontrun{
+#' library(httr)
+#' uri<- "https://api.mindat.org/geomaterials/?id__in=&hardness_min=9.3&fields=name,
+#' hardness&page_size=1500"
+#' api_token<- "9ce67655d74bcd981e937be80dcea9cb"
+#' response <- GET(uri,add_headers('Authorization'= paste('Token ',api_token,sep = "")))
+#' raw_data <- mindat_extract_response_body(response)
+#' }
 #' @export
 mindat_extract_response_body<-function(response){
   if (200 == status_code(response)){
@@ -128,6 +153,14 @@ mindat_extract_response_body<-function(response){
 #' @usage mindat_get_data_from_uri (uri)
 #' @param uri request uri
 #' @return df. R data frame of the request uri.
+#' @examples
+#' \dontrun{
+#' library(httr)
+#' uri <- "https://api.mindat.org/geomaterials/?id__in=&hardness_min=9.3&fields=name,
+#' hardness&page_size=1500"
+#' mindat_cache_set('api_token',"9ce67655d74bcd981e937be80dcea9cb")
+#' df <- mindat_get_data_from_uri(uri)
+#' }
 #' @export
 mindat_get_data_from_uri<-function(uri){
   if(mindat_cache_has('api_token')){
@@ -148,6 +181,15 @@ mindat_get_data_from_uri<-function(uri){
 #' @usage mindat_build_querystring (args)
 #' @param args query args.
 #' @return qs. generated query string.
+#' @examples
+#' \dontrun{
+#' mindat_cache_set('page_size',800)
+#' ids<-c("")
+#' hardness_min<- 9.3
+#' fields<- c("name,hardness")
+#' args<- list(ids,hardness_min,fields)
+#' querystring<-mindat_build_querystring(args)
+#' }
 #' @export
 mindat_build_querystring<-function(args){
   qs <- ''
